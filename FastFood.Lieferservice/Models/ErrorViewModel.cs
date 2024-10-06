3d29c80be7b23351c1846cbaf93aec6bd4e3fc9b
Aveen alhadad <aveen_alhadad@yahoo.com>
2024-10-06 09:47:21 +0200

    Projekt Schichte erstellt

---
+++ FastFood.Lieferservice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FastFood.Lieferservice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
